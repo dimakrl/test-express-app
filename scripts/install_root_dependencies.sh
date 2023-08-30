2914791c946675aa6b32976c4ca9f93a10f015cd
dimakrl <dmytro_kyrylenko@epam.com>
2023-08-30 13:10:50 +0300

    install codedeploy agent in root deps

--- scripts/install_root_dependencies.sh
+++ scripts/install_root_dependencies.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+sudo yum -y update
+
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16
+
+sudo yum -y install ruby
+sudo yum -y install wget
+sudo wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/test-express-app"
